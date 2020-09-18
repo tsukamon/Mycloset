467f8fd8ea02c73e1ae4d33f9a31c693aaa615cd
tsukamon <69575450+tsukamon@users.noreply.github.com>
2020-09-18 16:05:41 +0900

    投稿機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,31 @@
 class ItemsController < ApplicationController
   def index
+    if user_signed_in?
+      @item = Item.all.where(user_id: current_user.id)
+      @item = @item.order('created_at DESC')
+    end
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id]) 
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:explanation, :category_id, :season_id, :brand, :purchase_day, :price, :place, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  validates :category_id, :season_id, numericality: { other_than: 1, message: 'select' }
+  validates :image, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :season
+
+
 end
